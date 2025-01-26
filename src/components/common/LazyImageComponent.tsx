b3a55572952a157d2687c59ea967177ad230cbeb
sungjae_ahn <sungjae_ahn2@tmax.co.kr>
2025-01-26 23:05:55 +0900

    [feat] Optimize image & Dynamic Rendering

--- src/components/common/LazyImageComponent.tsx
+++ src/components/common/LazyImageComponent.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 interface LazyImageComponentProps {
   src: string;
@@ -13,27 +13,56 @@ interface LazyImageComponentProps {
 const LazyImageComponent: React.FC<LazyImageComponentProps> = ({
   src,
   alt,
-  width,
-  height,
+  width = 100,
+  height = 100,
   style,
   onClick,
 }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const imgRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true); // 뷰포트에 진입하면 로드
+          observer.disconnect(); // 한 번 로드 후 관찰 중지
+        }
+      },
+      { threshold: 0.1 } // 10%만 보이더라도 로드
+    );
+
+    if (imgRef.current) observer.observe(imgRef.current);
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <Image
-      src={src}
-      alt={alt}
-      width={width}
-      height={height}
-      loading="lazy" // Lazy Loading 속성 추가
-      style={{
-        objectFit: "contain",
-        width: `${width}px`,
-        height: `${height}px`,
-        ...style,
-      }} // 이미지 스타일 적용
-      onClick={onClick}
-      unoptimized
-    />
+    <div ref={imgRef} style={{ position: "relative", ...style }}>
+      {isVisible ? (
+        <Image
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          style={{
+            objectFit: "contain", width: `${width}px`,
+            height: `${height}px`,
+          }}
+          placeholder="blur" // 로드 전 블러 처리
+          blurDataURL="/loading-image-circle.gif" // 블러에 사용할 기본 이미지
+          onClick={onClick}
+        />
+      ) : (
+        <Image
+          src="/loading-image-circle.gif" // 로드 전 기본 이미지 표시
+          alt="Loading..."
+          width={width}
+          height={height}
+          style={{ objectFit: "contain" }}
+        />
+      )}
+    </div>
   );
 };
 
